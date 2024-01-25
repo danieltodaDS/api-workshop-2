0b7c4ab406ece2f6ccfbb7f4862848108808f4bb
daniel <hiroshi1991@gmail.com>
2024-01-25 16:23:21 -0300

    first commit

---
+++ testes.py
@@ -0,0 +1,18 @@
+import pytest
+
+# Funciona com um servidor de teste
+from fastapi.testclient import TestClient
+
+from main import app 
+
+client = TestClient(app)
+
+def test_ola_mundo():
+    response = client.get("/")
+    assert response.status_code == 200 
+
+def test_ola_mundo_json(): 
+    response = client.get("/")
+    
+    assert response.json() == {"ola":"mundo"}
+
