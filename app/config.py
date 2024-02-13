13ba5228183009abee0040d266a7994983a71e73
daniel <hiroshi1991@gmail.com>
2024-02-13 16:05:03 -0300

    config fixed

--- app/config.py
+++ app/config.py
@@ -12,7 +12,7 @@ db_name = os.getenv("POSTGRES_DB")
 db_host = os.getenv("DB_HOST")
 db_port = os.getenv("DB_PORT")
 
-DATABASE_URL = f"postgresql://{db_user}:{db_password}@{db_host}/{db_port}/{db_name}"
+DATABASE_URL = f"postgresql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
 
 engine = create_engine(DATABASE_URL)
 
@@ -31,4 +31,4 @@ def get_db():
 
 if __name__ == "__main__":
     db = get_db()
-    print (f"postgresql://{db_user}:{db_password}@{db_host}/{db_port}/{db_name}")
+    print (f"postgresql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}")
