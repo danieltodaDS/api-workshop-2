ce7483ae831fc461f867344af1f6c702c4078ed4
daniel <hiroshi1991@gmail.com>
2024-02-13 16:40:09 -0300

    testes db

--- tests/test_api.py
+++ tests/test_api.py
@@ -21,17 +21,3 @@ def test_lista_produtos_status_code():
 
     assert response.status_code == 200
 
-def test_tamanho_lista_produtos():
-    response = client.get("/produtos")
-
-    assert len(response.json()) == 3
-
-def test_pega_um_produto():
-    response = client.get("/produtos/1")
-
-    assert response.json() == {
-        "id": 1,
-        "nome":"Celular",
-        "descricao": "aparelho celular",
-        "preco": 2131
-    }
